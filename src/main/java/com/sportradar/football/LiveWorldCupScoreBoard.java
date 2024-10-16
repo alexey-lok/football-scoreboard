836a3d0ad6b26c680f435fee505afedf7271632a
Aleksei Loktionov <aleks.loktionov@gmail.com>
2024-10-16 13:30:17 +0200

    Added validations for starting the match

--- src/main/java/com/sportradar/football/LiveWorldCupScoreBoard.java
+++ src/main/java/com/sportradar/football/LiveWorldCupScoreBoard.java
@@ -1,8 +1,33 @@
 package com.sportradar.football;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LiveWorldCupScoreBoard {
 
+    private final Set<Match> matches;
+
+    public LiveWorldCupScoreBoard() {
+        this.matches = new HashSet<>();
+    }
+
     public void startMatch(String homeTeam, String awayTeam) {
+        if (homeTeam == null || awayTeam == null || homeTeam.isEmpty() || awayTeam.isEmpty()) {
+            throw new IllegalArgumentException("Team names cannot be null or empty");
+        }
+
+        if (homeTeam.equals(awayTeam)) {
+            throw new IllegalArgumentException("Home and away teams cannot be the same");
+        }
+
+        Match match = new Match(homeTeam, awayTeam);
+        if (matches.contains(match)) {
+            throw new IllegalStateException("Match " + homeTeam + "-" + awayTeam + " already started");
+        }
+        matches.add(match);
+    }
+
+    private record Match(String homeTeam, String awayTeam) {        
     }
 
 }
--- src/test/java/com/sportradar/football/LiveWorldCupScoreBoardTest.java
+++ src/test/java/com/sportradar/football/LiveWorldCupScoreBoardTest.java
@@ -2,11 +2,56 @@ package com.sportradar.football;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class LiveWorldCupScoreBoardTest {
     
     @Test
-    void testCanStartMatch() {
+    void canStartMatch() {
         LiveWorldCupScoreBoard scoreboard = new LiveWorldCupScoreBoard();
         scoreboard.startMatch("Mexico", "Canada");
+    }
+
+    @Test
+    void startMatchWithEmptyTeamNamesThrowsIllegalArgumentException() {
+        LiveWorldCupScoreBoard scoreboard = new LiveWorldCupScoreBoard();
+        String expectedMessage = "Team names cannot be null or empty";
+        assertThatThrownBy(() -> {
+            scoreboard.startMatch(null, null);
+        })
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(expectedMessage);
+
+        assertThatThrownBy(() -> {
+            scoreboard.startMatch("", "");
+        })
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(expectedMessage);
+    }
+
+    @Test
+    void startMatchWithSameHomeAndAwayTeamThrowsIllegalArgumentException() {
+        LiveWorldCupScoreBoard scoreboard = new LiveWorldCupScoreBoard();
+        String expectedMessage = "Home and away teams cannot be the same";
+        assertThatThrownBy(() -> {
+            scoreboard.startMatch("team", "team");
+        })
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(expectedMessage);
+    }
+
+    @Test
+    void canNotStartTheSameMatchTwice() {
+        LiveWorldCupScoreBoard scoreboard = new LiveWorldCupScoreBoard();
+        
+        scoreboard.startMatch("Mexico", "Canada");
+        
+        String expectedMessage = "Match Mexico-Canada already started";
+        
+        assertThatThrownBy(() -> {
+            scoreboard.startMatch("Mexico", "Canada");
+        })
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessage(expectedMessage);
     }
 }
